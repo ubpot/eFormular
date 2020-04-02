3fb13cd0536026496280d4be1ee4217f504c3d1d
MaximilianKleissl <47460369+MaximilianKleissl@users.noreply.github.com>
2020-04-02 10:06:04 +0200

    Create eFormAPI.js

---
+++ eFormAPI.js
@@ -0,0 +1,21 @@
+// JavaScript Document
+/*
+
+Autor: Paul Borchert
+Date : Dez 2010
+
+*/
+
+
+function eF_API_getValueByFormulartyp(Formulartyp,htmlElementId) {
+  if (! MyEFormValues[Formulartyp]) throw "Formulartyp in dem Ordner nicht gefunden";
+  if (MyEFormValues[Formulartyp][htmlElementId]) {                                         
+    Value = MyEFormValues[Formulartyp][htmlElementId];
+    Value = Value.replace(/\\u0027/g,"'");
+    Value = Value.replace(/\\u0022/g,'"');
+    return Value;
+  } else {
+    //throw "Id in dem Formulartyp nicht gefunden.";
+    return null;
+  }
+}
